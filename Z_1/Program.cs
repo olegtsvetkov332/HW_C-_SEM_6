736bc3f2cb183de9aa8f2641d0b752876111c178
olegtsvetkov332 <tsvetkovoleg798@gmail.com>
2023-06-08 15:59:29 +0300

    дз семинар 6

---
+++ Z_1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+System.Console.Write("Введите длинну массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] GenerateArray(int Lenght)
+{
+    int[] Array = new int[Lenght];
+    for (int i = 0; i != Lenght; i++)
+    {
+        System.Console.Write($"Введите {i} элемент массива: ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return Array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("Исходный массив [");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.Write("] чисел больше нуля: ");
+}
+
+int PositiveDigits(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    return counter;
+}
+
+int[] Array = GenerateArray(len);
+PrintArray(Array);
+Console.WriteLine(PositiveDigits(Array));
---
+++ Z_2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Введите точку b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите точку k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите точку b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите точку k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double PoiskX(double arg1, double arg2, double arg3, double arg4)
+{
+    double x = ((arg3 - arg1) / (arg2 - arg4));
+    return x;
+}
+double PoiskY(double arg1, double arg2, double arg3)
+{
+    double y = arg1 * arg2 + arg3;
+    return y;
+}
+if((b1/b2)!=(k1/k2))
+{
+    Console.Write($"Точка пересечения двух прямых -> ({PoiskX(b1, k1, b2, k2)};");
+    Console.Write($"{PoiskY(k1, PoiskX(b1, k1, b2, k2), b1)})");
+}
+else 
+{
+    Console.Write("Прямые параллельны либо не совпадают.");
+}
